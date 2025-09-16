3b1e268c26a108cc9993ca0af8f4d9c850a535f9
William Giraldo <william.giraldo@cognosos.com>
2025-09-16 06:21:35 +0200

    finish day 21

--- 21a.js
+++ 21a.js
@@ -1,11 +1,5 @@
 const fs = require('fs');
 const s = fs.readFileSync('./input.txt').toString();
-// const s = `3   4
-// 4   3
-// 2   5
-// 1   3
-// 3   9
-// 3   3`;
 
 function parseInput(str) {
     return str.split('\n');
@@ -25,26 +19,26 @@ const NUMERIC_KEYPAD = {
 };
 
 const PRECOMPUTED = {
-    'A-^': '<',
-    'A->': 'v',
-    'A-v': '<v', // Change this
-    'A-<': 'v<<',
-    '^-A': '>',
-    '^->': 'v>',
-    '^-v': 'v',
-    '^-<': 'v<',
-    '>-A': '^',
-    '>-^': '^<',
-    '>-v': '<',
-    '>-<': '<<',
-    'v-A': '^>',
-    'v-^': '^',
-    'v->': '>',
-    'v-<': '<',
-    '<-A': '>>^' ,
-    '<-^': '>^',
-    '<->': '>>',
-    '<-v': '>'
+    'A-^': ['<A'],
+    'A->': ['vA'],
+    'A-v': ['<vA', 'v<A'],
+    'A-<': ['v<<A', '<v<A'],
+    '^-A': ['>A'],
+    '^->': ['v>A', '>vA'],
+    '^-v': ['vA'],
+    '^-<': ['v<A'],
+    '>-A': ['^A'],
+    '>-^': ['^<A', '<^A'],
+    '>-v': ['<A'],
+    '>-<': ['<<A'],
+    'v-A': ['^>A', '>^A'],
+    'v-^': ['^A'],
+    'v->': ['>A'],
+    'v-<': ['<A'],
+    '<-A': ['>>^A', '>^>A'] ,
+    '<-^': ['>^A'],
+    '<->': ['>>A'],
+    '<-v': ['>A']
 };
 
 function getPathsForNumeric(str, startPos) {
@@ -75,105 +69,48 @@ function getPathsForNumeric(str, startPos) {
     }
 }
 
-// function getPathsForKeypad(str, startPos) {
-//     let paths = [''];
-//     for (let c of str) {
-//         if (startPos === c) {
-//             paths = paths.map(p => p + 'A');
-//         } else {
-//             const pc = PRECOMPUTED[`${startPos}-${c}`];
-//             if (!pc) {
-//                 throw new Error(`Entry not found for ${startPos} - ${c}`);
-//             }
+const NUM_KEYPADS = 26;
+let CACHE = Array(NUM_KEYPADS - 2);
 
-//             paths = paths.map((p) => pc.map(pre => p + pre + 'A')).flat();
-//         }
-//         startPos = c;
-        
-//     }
-//     return paths;
-// }
+function calcTransition(dst, org, l) {
+    const key = `${org}-${dst}`;
 
-function getPathsForKeypad(str, startPos) {
-    let path = '';
-    for (let c of str) {
-        if (startPos === c) {
-            path += 'A';
-        } else {
-            const pc = PRECOMPUTED[`${startPos}-${c}`];
-            if (!pc) {
-                throw new Error(`Entry not found for ${startPos} - ${c}`);
-            }
-
-            path += pc + 'A';
-        }
-        startPos = c;
-        
+    if (dst === org) return 1;
+    if (l == NUM_KEYPADS - 1){
+        return PRECOMPUTED[key][0].length;
     }
-    return path;
+
+    if (!CACHE[l - 1]) {
+        CACHE[l - 1] = {};
+    }
+
+    if (key in CACHE[l - 1]) {
+        return CACHE[l - 1][key];
+    }
+
+    const res = Math.min(...PRECOMPUTED[key].map(path => calcString(path, l + 1)));
+    CACHE[l - 1][key] = res;
+    return res;
 }
 
-const NUM_KEYPADS = 3;
-function alg(codes) {
-    const target = Math.ceil(NUM_KEYPADS / 2);
-    const symbols = '<>^vA';
-
-    const pre = [];
-    for (let i = 0; i < target; i++) {
-        const O = {};
-        for (let s1 of symbols) {
-            for (let s2 of symbols) {
-                if (i === 0) {
-                    O[`${s1}-${s2}`] = getPathsForKeypad(s2, s1);
-                } else {
-                    O[`${s1}-${s2}`] = getPathsForKeypad(pre[i - 1][`${s1}-${s2}`], 'A');
-                }
-            }
-        }
-        pre.push(O);
-    }
-
-    const left = pre[target - 1];
-    const right = pre[NUM_KEYPADS - target - 1];
-
+function calcString(str, l) {
     let s = 0;
-    codes = ['029A'];
-    for (let code of codes) {
-        const paths = [
-            'v'
-            //getPathsForNumeric(code, [3, 2])[0]
-        ];
-
-        console.log(paths);
-        let sa = Infinity;
-        paths.forEach((path) => {
-            let s1 = 0;
-            let last1 = 'A';
-            let val = '';
-            for (let c1 of path) {
-                let last2 = 'A';
-                for (let c2 of left[`${last1}-${c1}`]) {
-                    s1 += right[`${last2}-${c2}`].length;
-                    val += right[`${last2}-${c2}`];
-                    last2 = c2;
-                }
-                last1 = c1;
-            }
-            sa = Math.min(sa, s1);
-
-            for (let k = 0; k < NUM_KEYPADS; k++) {
-                path = getPathsForKeypad(path, 'A');
-                console.log(path);
-            }
-            
-        });
-        s = sa;
-        // s += code.split('').filter(v => /\d/.test(v)).join('') * sa;
+    let prev = 'A';
+    for (let c of str) {
+        const t = calcTransition(c, prev, l);
+        s += t;
+        prev = c;
     }
-    
-    
     return s;
 }
 
+function alg(codes) {
+    let s = 0;
+    for (let code of codes) {
+        const sa = Math.min(...getPathsForNumeric(code, [3, 2]).map(path => calcString(path, 1)));
+        s += code.split('').filter(v => /\d/.test(v)).join('') * sa;
+    }
+    return s;
+}
 
 console.log(alg(parseInput(s)));
